4911e3007f0ee995ce035600c990173fbee82604
Indigo9898 <33042992+Indigo9898@users.noreply.github.com>
2023-08-11 20:37:58 -0500

    Update SkillBtn.jsx

--- app/components/SkillBtn.jsx
+++ app/components/SkillBtn.jsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const SkillBtn = (props) => {
     return(
-        <div className="h-8 w-fit p-2 pr-6 pl-6 rounded-3xl bg-btn-color flex-wrap mr-4 flex items-center justify-center mb-4">
+        <div className="h-8 w-fit p-2 pr-6 pl-6 rounded-3xl bg-btn-color flex-wrap mr-4 flex items-center justify-center whitespace-nowrap mb-4">
             <p className="text-xs">{props.skill}</p>
         </div>
     )
